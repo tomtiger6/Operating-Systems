b2ca1b61ab58caa6eb49c7829b61f089d3f25ac5
tomtiger6 <tomtiger6@gmail.com>
2022-11-25 23:00:12 +0200

    Add files via upload

---
+++ smash.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include "Commands.h"
+#include "signals.h"
+#include <string>
+
+#define MAX_COMMAND 81
+std::string command_prompt[MAX_COMMAND] = "smash";
+
+int main(int argc, char* argv[]) {
+    if(signal(SIGTSTP , ctrlZHandler)==SIG_ERR) {
+        perror("smash error: failed to set ctrl-Z handler");
+    }
+    if(signal(SIGINT , ctrlCHandler)==SIG_ERR) {
+        perror("smash error: failed to set ctrl-C handler");
+    }
+
+
+    //TODO: setup sig alarm handler
+
+    SmallShell& smash = SmallShell::getInstance();
+    while(true) {
+        std::cout << command_prompt << ">";
+        std::string cmd_line;
+        std::getline(std::cin, cmd_line);
+        smash.executeCommand(cmd_line.c_str());
+    }
+    return 0;
+}
